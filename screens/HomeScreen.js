46d6434acbf07728c861cb0a5f47c211925af626
sonhai32 <sonhaizeros@gmail.com>
2021-03-14 22:05:36 +0700

    🧚🏻‍♀️ add Stack Screen ScheduleScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,10 +7,12 @@ const Stack = createStackNavigator()
 
 import DashboardSceen from './DashboardSceen'
 import ProfileScreen from './ProfileScreen'
+import ScheduleScreen from './ScheduleScreen'
 const HomeScreen  = () =>(
     <Stack.Navigator headerMode="none">
         <Stack.Screen component={DashboardSceen} name='Dashboard' />
         <Stack.Screen component={ProfileScreen} name='Profile' />
+        <Stack.Screen component={ScheduleScreen} name='Schedule' />
     </Stack.Navigator>
 )
 
