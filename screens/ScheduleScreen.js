e5a85bad09d3632451407204d22db75c0d2351de
sonhai32 <sonhaizeros@gmail.com>
2021-03-15 20:10:52 +0700

    remove func 'getAllDaysInWeek' because unused

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -10,48 +10,7 @@ import AppBar from '../components/Layouts/AppBar';
 
 import {COLORS, SIZES} from '../constants/theme';
 const ScheduleScreen = ({navigation}) => {
-  const getAllDayInWeek = (date) => {
-    const DAY_TO_SECONDS = 24 * 60 * 60;
-
-    //customize first day of week is Monday
-
-    const getFirstDay = (dayUnix, prevDay) => {
-      return dayUnix - (prevDay - 1) * DAY_TO_SECONDS;
-      // prevDay -1 | Sunday begin at 0
-    };
-
-    const getNextDay = (step, dayUnix) => {
-      return dayUnix + step * DAY_TO_SECONDS;
-    };
-
-    const isOutOfWeek = (weekIndex, dayUnix) => {
-      return moment.unix(dayUnix).utc(true).week() !== weekIndex;
-    };
-    const currentDate = moment(date, 'DD/MM/YYYY');
-    const firstDate =
-      currentDate.day() == 0
-        ? getFirstDay(currentDate.unix(), 7)
-        : currentDate.day() == 1
-        ? currentDate.unix()
-        : getFirstDay(currentDate.unix(), currentDate.day());
-    console.log(firstDate);
-    const weekIndex = moment.unix(firstDate).utc(true).week();
-
-    const allDayInWeek = [];
-    for (let i = 0; i < 7; i++) {
-      const nextDate = getNextDay(i, firstDate);
-
-      if (isOutOfWeek(weekIndex, nextDate)) {
-        break;
-      } else {
-        allDayInWeek.push(moment.unix(nextDate).utc(true).format('DD/MM/YYYY'));
-      }
-    }
-    console.log(allDayInWeek);
-
-    return allDayInWeek;
-  };
-
+  
   
   const styles = StyleSheet.create({
     container: {
