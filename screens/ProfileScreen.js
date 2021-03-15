ba510c03da78c9845f0c2b1ee3599b90620411e3
sonhai32 <sonhaizeros@gmail.com>
2021-03-15 18:47:40 +0700

    🌵 add AppBar component to ProfileScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import {COLORS, SIZES} from '../constants/theme';
 
 import {FlatGrid} from 'react-native-super-grid';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import AppBar from '../components/Layouts/AppBar';
 import LinearGradient from 'react-native-linear-gradient';
 
 import {
@@ -23,14 +23,14 @@ const ProfileScreen = ({navigation}) => {
     container: {
       flex: 1,
       display: 'flex',
-      position: 'relative'
+      position: 'relative',
     },
     overlayContainer: {
       flex: 1,
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      position: 'relative'
+      position: 'relative',
     },
     avatarContainer: {
       flex: 1,
@@ -38,68 +38,87 @@ const ProfileScreen = ({navigation}) => {
       flexDirection: 'column',
       alignItems: 'center',
       marginTop: SIZES.padding * 10,
-      
     },
-    avatar:{
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        resizeMode: 'cover'
+    avatar: {
+      width: 200,
+      height: 200,
+      borderRadius: 100,
+      resizeMode: 'cover',
     },
     userTextName: {
-        fontSize: SIZES.h1,
-        color: COLORS.blue,
-        fontWeight: '200',
-        marginTop: 10
-        
+      fontSize: SIZES.h1,
+      color: COLORS.blue,
+      fontWeight: '200',
+      marginTop: 10,
     },
     infoContainer: {
-        flex: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        padding: SIZES.padding * 2,
+      flex: 2,
+      display: 'flex',
+      flexDirection: 'column',
+      padding: SIZES.padding * 2,
     },
-    infoItems:{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
+    infoItems: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     infoItemsLabel: {
-        flex: 1,
-        color: COLORS.black,
-        fontSize: SIZES.h4,
-        fontWeight: '500'
+      flex: 1,
+      color: COLORS.black,
+      fontSize: SIZES.h4,
+      fontWeight: '500',
     },
     infoItemsData: {
       flex: 2.5,
-      color: COLORS.black
+      color: COLORS.black,
     },
-    btnBack:{
+    btnBack: {
       display: 'flex',
       alignItems: 'center',
       flexDirection: 'row',
       position: 'absolute',
       top: 50,
-      left: 20
-    }
+      left: 20,
+    },
   });
-  const RenderUserInfo = (user) =>{
-return Object.keys(user).map((val, key)=>{
-return(
-              <View key={`v-${val}`} style={styles.infoItems}>
-                <Text key={`t-${val}`} style={styles.infoItemsLabel}> {val === 'id' ? 'MSSV: ': val === 'name' ? 'Họ và tên: ' : val === 'birthDate' ? 'Ngày sinh: ': val === 'place' ? 'Nơi sinh: ' : val === 'gender' ? 'Giới tính: ' : val === 'email' ? 'Email: ' : val === 'emailUEH' ? 'Email UEH: ' : val === 'avatar' ? '' : ''} </Text> 
-                <Text key={`user-${val}`} style={styles.infoItemsData}> {val === 'avatar' ? null : user[val]} </Text> 
-              </View>
-          )
-      })
-  }
-  const  test = (user) =>{
-      
-    for (const [key, value] of Object.entries(user)){
-        console.log(`${key}: ${value}`)
+  const RenderUserInfo = (user) => {
+    return Object.keys(user).map((val, key) => {
+      return (
+        <View key={`v-${val}`} style={styles.infoItems}>
+          <Text key={`t-${val}`} style={styles.infoItemsLabel}>
+            {' '}
+            {val === 'id'
+              ? 'MSSV: '
+              : val === 'name'
+              ? 'Họ và tên: '
+              : val === 'birthDate'
+              ? 'Ngày sinh: '
+              : val === 'place'
+              ? 'Nơi sinh: '
+              : val === 'gender'
+              ? 'Giới tính: '
+              : val === 'email'
+              ? 'Email: '
+              : val === 'emailUEH'
+              ? 'Email UEH: '
+              : val === 'avatar'
+              ? ''
+              : ''}{' '}
+          </Text>
+          <Text key={`user-${val}`} style={styles.infoItemsData}>
+            {' '}
+            {val === 'avatar' ? null : user[val]}{' '}
+          </Text>
+        </View>
+      );
+    });
+  };
+  const test = (user) => {
+    for (const [key, value] of Object.entries(user)) {
+      console.log(`${key}: ${value}`);
     }
-  } 
+  };
   return (
     <UserContext.Consumer>
       {(user) => (
@@ -120,20 +139,19 @@ return(
               start={{x: 1, y: 0}}
               end={{x: 0.4, y: 1}}
               style={styles.container}>
-                  <TouchableOpacity style={styles.btnBack} onPress = {() => navigation.navigate('Dashboard')}>
-                    <Ionicons name='arrow-back-outline' color={COLORS.black} size={26} />
-                    <Text style={{color: COLORS.black, fontSize: SIZES.font}}>Trở vê</Text>
-                  </TouchableOpacity>
+              <AppBar
+                navigation={navigation}
+                navigate={'Dashboard'}
+                title={'Thông tin cá nhân'}
+              />
               <View style={styles.avatarContainer}>
                 <Image
                   source={{uri: user.userInfo.avatar}}
                   style={styles.avatar}
                 />
-                <Text style={styles.userTextName}>
-                    {user.userInfo.name}
-                </Text>
+                <Text style={styles.userTextName}>{user.userInfo.name}</Text>
               </View>
-              <View style={styles.infoContainer}> 
+              <View style={styles.infoContainer}>
                 {RenderUserInfo(user.userInfo)}
               </View>
             </LinearGradient>
