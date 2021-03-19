78dfdc09ec136ba073561639630f66c0c2e05755
sonhai32 <sonhaizeros@gmail.com>
2021-03-19 14:45:51 +0700

    🌟 add CORS to api modules

--- modules/API.js
+++ modules/API.js
@@ -22,9 +22,13 @@ class UEH_API {
         "connection": "keep-alive",
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+          'Access-Control-Allow-Origin': '*',
+
       },
+      mode: 'cors',
       jar: this.jar,
       withCredentials: true,
+    
     });
 
     axiosCookieJarSupport(this.request);
