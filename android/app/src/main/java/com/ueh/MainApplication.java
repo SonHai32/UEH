6ce7e94e9cb16773aa65e4f3940b976da42cd688
sonhai32 <sonhaizeros@gmail.com>
2021-03-23 00:01:05 +0700

    🌕 add codepush config android

--- android/app/src/main/java/com/ueh/MainApplication.java
+++ android/app/src/main/java/com/ueh/MainApplication.java
@@ -15,6 +15,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -43,6 +45,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSBundleFile() {
             return CodePush.getJSBundleFile();
         }
+
+        
       };
 
   @Override
